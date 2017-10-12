9b1559c3e79f66f07b834d0f7ec64a107200ce26
Peter Hozak <peter.hozak@merck.com>
2017-10-12 16:14:24 +0200

    lesson-1 react v2 static elements

--- lesson-1/react/current/src/App.js
+++ lesson-1/react/current/src/App.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <button>Red</button>
+        <button>Blue</button>
+        <div className="App-output App-output1" />
+        <div className="App-output App-output2">Lorem ipsum, ...</div>
       </div>
     );
   }
