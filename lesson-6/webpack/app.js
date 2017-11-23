877156f68a3a8daec2d5ab9446d5188a918ae734
Jan Bárta <jan.barta@merck.com>
2017-11-23 17:52:54 +0100

    Prepare for live coding

--- lesson-6/webpack/app.js
+++
@@ -1,6 +0,0 @@
-import sayHello from './hello'
-import './styles.css'
-import logo from './logo.png'
-
-document.getElementById('name').innerText = sayHello('Jan')
-document.getElementById('logo').src = logo
--- lesson-6/webpack/hello.js
+++
@@ -1,3 +0,0 @@
-export default (name) => {
-  return `Hello ${name}`
-}
